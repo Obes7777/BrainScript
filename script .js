7efb9626c435026b7beeb6d80e9ea6d7b2270c64
Obes7777 <78672497+Obes7777@users.noreply.github.com>
2021-05-11 18:53:19 -0500

    Update script .js

--- script .js
+++ script .js
@@ -1,25 +1,27 @@
-function random(min, max){
+//random integer
+function randomInt(min, max){
     return Math.floor(Math.random() * (max - min + 1) ) + min
 }
 
 
-
+//ask a question
 function ask(message){
     return prompt(message)
 }
 
 
-
+//edit the text of an html tag
 function changeText(id, message){
     document.getElementById(id).innerHTML = message
 }
 
 
-
+//hides an element
 function hide(id){
     document.getElementById('option1').style.visibility = 'hidden'
 }
 
+//shows an element
 function show(id){
     document.getElementById('option1').style.visibility = 'visible'
 }
